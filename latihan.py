6fad2d4006ca320d19bec03ebe2a467af882d0a4
epo33m <rivoldyvoldy@gmail.com>
2026-04-17 03:23:19 +0800

    Add latihan.py

---
+++ latihan.py
@@ -0,0 +1,13 @@
+print("=== PROGRAM DERET ANGKA ===")
+
+# Ini untuk mendefinisikan batas
+a = 10 
+
+# Kita lakukan perulangan
+for x in range(1, 11):
+    # Cek kalau ganjil
+    if x % 2 == 1:
+        # Cetak outputnya
+        print("Ini angka ganjil:", x)
+
+# Selesai
